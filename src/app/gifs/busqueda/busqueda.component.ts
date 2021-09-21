72c99e1476c738fc429199cdfde05e375cee1f99
Kilian Perez <kilianperez92@gmail.com>
2021-09-21 19:20:52 +0200

    @ViewChild - Obtener referencias a objetos del HTML
    Creamos un viewChild en el que le quitamos le adjudicamos y reseteamos el valor al hacer entrer en el teclado con el keyup

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  // el ViewChild es como el docmuent.querySelecto, busca una refencia local que tenga #
 
-  constructor() { }
+  // non-null assertion operator se asegura que el objeto no es nulo
 
-  ngOnInit(): void {
+  // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+    // buscar(termino: string) {
+    // console.log(termino);
+    // console.log(this.txtBuscar);
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+
+    // vaciar el valor del Input como en js pero mejor con el @viewChild
+    // document.querySelector('input').value = '';
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
