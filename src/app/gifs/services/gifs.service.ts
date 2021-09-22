aa8eae306dcad90acb8ae23291ef883d7dccda42
Kilian Perez <kilianperez92@gmail.com>
2021-09-22 11:57:48 +0200

    Realizar una petición HTTP
    1- Añadimos la clave de la API
    2- Importamos el modulo HttpClientModule en el app.module.ts para hacer la petición a la API
    3- Hacemos la petición a la API

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,34 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  // con este inyectable el servicio esta de forma global, si no queremos que asi sea lo tenemos que importar en el correspondiente modulo 
-  providedIn: 'root'
+  // con este inyectable el servicio esta de forma global, si no queremos que asi sea lo tenemos que importar en el correspondiente modulo
+  providedIn: 'root',
 })
 export class GifsService {
-  // lo iniciamos de forma privada y por eso le añadimos el guión bajo  
-  private _historial: string[] =[]
+  // api de https://developers.giphy.com/
+  private apiKey: string = 'KbTxm2B0Dp7cATeK7Hf2KVRi8kWo79g3';
 
-  get historial(){
+  // lo iniciamos de forma privada y por eso le añadimos el guión bajo
+  private _historial: string[] = [];
 
-
-    // [...]creamos un nuevo array 
-    return [...this._historial]
+  get historial() {
+    // [...]creamos un nuevo array
+    return [...this._historial];
   }
-  buscarGifs(query: string = ''){
-    
-    query = query.trim().toLocaleLowerCase()
+  constructor(private http : HttpClient){}
+  buscarGifs(query: string = '') {
+    query = query.trim().toLocaleLowerCase();
 
     // hacer que sean unicos los valores que se añadan al array
     if (!this._historial.includes(query)) {
-      this._historial.unshift(query)
-      
+      this._historial.unshift(query);
+      // evitar que se añadan mas de 10 elementos en el array
+      this._historial = this._historial.splice(0, 10);
     }
+    // console.log(this._historial);
 
-    // evitar que se añadan mas de 10 elementos en el array 
-    this._historial = this._historial.splice(0, 10);
+    /*
+    // llamada a la api con js
+    fetch(
+      'https://api.giphy.com/v1/gifs/search?api_key=KbTxm2B0Dp7cATeK7Hf2KVRi8kWo79g3&q=dragon ball z&limit=10'
+    ).then((resp) => {
+      resp.json().then((data) => console.log(data));
+    });
 
-
-    console.log(this._historial);
-    
+    // tambien puede ser con un Fetch with async/await añadiendo a esta funcion buscarGisfs en async y con lo siguiente
+    */
+    /*
+    const resp = await fetch(
+      'https://api.giphy.com/v1/gifs/search?api_key=KbTxm2B0Dp7cATeK7Hf2KVRi8kWo79g3&q=dragon ball z&limit=10'
+    )
+    const data = await resp.json();
+    console.log(data);
+    */
+    this.http.get(
+      'https://api.giphy.com/v1/gifs/search?api_key=KbTxm2B0Dp7cATeK7Hf2KVRi8kWo79g3&q=dragon ball z&limit=10'
+    ).subscribe((resp: any) =>{
+      console.log(resp.data);
+      
+    });
   }
 }
